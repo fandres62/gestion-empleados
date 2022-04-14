8c1a975068acb388b01f23c27f3b1ca3e769a420
fandres62 <103400084+fandres62@users.noreply.github.com>
2022-04-14 09:46:27 -0500

    Add files via upload

---
+++ gestion-empleados-frontend/src/app/registrar-empleado/registrar-empleado.component.ts
@@ -0,0 +1,38 @@
+import  swal  from 'sweetalert2';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Empleado } from '../empleado';
+import { EmpleadoService } from '../empleado.service';
+
+@Component({
+  selector: 'app-registrar-empleado',
+  templateUrl: './registrar-empleado.component.html',
+  styleUrls: ['./registrar-empleado.component.css']
+})
+export class RegistrarEmpleadoComponent implements OnInit {
+
+  empleado: Empleado = new Empleado();
+
+  //router llamar a la ruta
+  constructor(private empleadoServicio:EmpleadoService,private router:Router) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  guardarEmpleado(){
+    this.empleadoServicio.registrarEmpleado(this.empleado).subscribe(dato => {
+      console.log(dato);
+      this.irALaListaDeEmpleados();
+    },error => console.log(error));
+  }
+
+  irALaListaDeEmpleados(){
+    this.router.navigate(['/empleados']);
+    swal('Empleado registrado',`El empleado ${this.empleado.nombre} ha sido registrado con exito`,`success`);
+  }
+
+  onSubmit(){
+    this.guardarEmpleado();
+  }
+}
